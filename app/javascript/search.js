eb2cb6dab2d9deb1ef822240058ef8c3f315c269
Ngani Faith <nganifaith@gmail.com>
2024-01-05 23:39:21 +0100

    realtime search done

---
+++ app/javascript/search.js
@@ -0,0 +1,27 @@
+const searchForm = document.getElementById("search-form");
+const resultsContainer = document.getElementById("results");
+
+function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
+
+function search() {
+    const value = searchForm.search.value;
+    if(value) {
+        fetch(`/search?search_term=${value}`)
+        .then(res => res.text())
+        .then(res => {
+            resultsContainer.innerHTML = res;
+        });
+    }
+}
+
+searchForm.search.addEventListener("keyup", debounce(search, 500));
+searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    search();
+});
