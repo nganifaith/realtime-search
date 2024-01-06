e970ee8a42c194f38d728236ab809aaae133f670
Ngani Faith <nganifaith@gmail.com>
2024-01-06 14:53:51 +0100

    Add loading indicator

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,5 +1,6 @@
 const searchForm = document.getElementById("search-form");
 const resultsContainer = document.getElementById("results");
+const loader = document.getElementById("loader");
 
 function debounce(func, timeout = 300){
     let timer;
@@ -12,11 +13,13 @@ function debounce(func, timeout = 300){
 function search() {
     const value = searchForm.search.value;
     if(value) {
+        loader.classList.remove("hidden");
         fetch(`/search?search_term=${value}`)
-        .then(res => res.text())
-        .then(res => {
-            resultsContainer.innerHTML = res;
-        });
+            .then(res => res.text())
+            .then(res => {
+                loader.classList.add("hidden");
+                resultsContainer.innerHTML = res;
+            });
     }
 }
 
